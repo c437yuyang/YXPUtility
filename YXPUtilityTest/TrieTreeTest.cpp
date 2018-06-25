5255132ffe9d726ca7149557fcbf737e155038d2
c437yuyang <954222619@qq.com>
2018-06-25 17:51:12 +0800

    add test for TrieTree

--- YXPUtilityTest/TrieTreeTest.cpp
+++ YXPUtilityTest/TrieTreeTest.cpp
@@ -5,28 +5,37 @@ using namespace yxp_utility;
 
 TEST(TrieTreeTest, demo1)
 {
-	/*TrieTree trie;
-	std::cout << trie.count("zuo") << std::endl;
+	TrieTree trie;
+	EXPECT_EQ(trie.count("zuo"), 0);
 	trie.insert("zuo");
-	std::cout << trie.count("zuo") << std::endl;
+	EXPECT_EQ(trie.count("zuo"), 1);
 
 	trie.remove("zuo");
-	std::cout << trie.count("zuo") << std::endl;
+	EXPECT_EQ(trie.count("zuo"), 0);
 	trie.insert("zuo");
 	trie.insert("zuo");
 	trie.remove("zuo");
-	std::cout << trie.count("zuo") << std::endl;
-
+	EXPECT_EQ(trie.count("zuo"), 1);
 	trie.remove("zuo");
-	std::cout << trie.count("zuo") << std::endl;
+	EXPECT_EQ(trie.count("zuo"), 0);
 
 	trie.insert("zuoa");
 	trie.insert("zuoac");
 	trie.insert("zuoab");
 	trie.insert("zuoad");
 	trie.remove("zuoa");
-	std::cout << trie.count("zuoa") << std::endl;
-	std::cout << trie.prefixCount("zuo") << std::endl;*/
+	EXPECT_EQ(trie.count("zuo"), 0);
+	EXPECT_EQ(trie.prefixCount("zuo"), 3);
 
+	trie.clear();
+	EXPECT_EQ(trie.count("zuo"), 0);
+	EXPECT_EQ(trie.prefixCount("zuo"), 0);
 
+	trie.insert("zuoa");
+	trie.insert("zuoac");
+	trie.insert("zuoab");
+	trie.insert("zuoad");
+	trie.remove("zuoa");
+	EXPECT_EQ(trie.count("zuo"), 0);
+	EXPECT_EQ(trie.prefixCount("zuo"), 3);
 }
