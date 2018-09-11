7987dd4a1d7ea83e01e5906e9f272c93c08a4e1c
c437yuyang <954222619@qq.com>
2018-09-11 09:16:33 +0800

    修改auto_ptr,增加operator!() 去掉operator auto_ptr<U>()(泛型copying函数可以替代)

--- Utilities/auto_ptr.h
+++ Utilities/auto_ptr.h
@@ -21,8 +21,13 @@ namespace yxp_utility {
 		T* operator->() const { return pointer_; } //这里返回值是另外一个对象了，所以不需要返回值是const，里面的pointer_是始终不会受影响的
 												   //这里就是调用的时候 obj->func() ,返回指针然后指针的内置操作->
 
+		bool operator!() const { //重载operator! 方便判断表达式使用
+			return pointer_ == nullptr;
+		}
+
+
 		T* get() const { return pointer_; }
-		T* release() { T*p = get(); pointer_ = NULL; return p; }
+		T* release() const { T*p = get(); pointer_ = nullptr; return p; }
 		void reset(T* pointer) {
 			if (pointer_)
 				delete pointer_;
@@ -30,19 +35,19 @@ namespace yxp_utility {
 		}
 
 		//原始copy ctor
-		auto_ptr(auto_ptr<T> &other) :pointer_(other.release()) {}
+		auto_ptr(const auto_ptr<T> &other) :pointer_(other.release()) {}
 
 		//内置指针是可以直接隐式转换的(父类指向子类),但是auto_ptr<base>和auto_ptr<derived>是两个不同的类根本就
 		//然后auto_ptr想要实现的话，必须利用function templates,也就是class生成函数或叫做泛化构造函数)
 		template<class U>
-		auto_ptr(auto_ptr<U>& other) //但这样一来，子类指针也可以指向一个父类对象，因此要利用初始值列表来实现对所创建的"成员函数群"实现挑拣
+		auto_ptr(const auto_ptr<U>& other) //但这样一来，子类指针也可以指向一个父类对象，因此要利用初始值列表来实现对所创建的"成员函数群"实现挑拣
 			: pointer_(other.release()) //这里要求存在一个隐式转换，能够使U*能够隐式转换成T*才能通过编译，就实现了必须是子类向父类的转换
 		{//这里因为是auto_ptr，所以不能用const
 
 		}
 
 		//原始的copy assignment
-		auto_ptr<T>& operator=(auto_ptr<T>&other) {
+		auto_ptr<T>& operator=(const auto_ptr<T>&other) {
 			if (this != &other) {//这个版本可以这样检测自赋值
 				reset(other.release());
 			}
@@ -50,15 +55,21 @@ namespace yxp_utility {
 		}
 
 		template<class U>
-		auto_ptr<T>& operator=(auto_ptr<U>& other) {
+		auto_ptr<T>& operator=(const auto_ptr<U>& other) {
 			//if (this != &other) //这里不能要这一句，书上的代码都是错的？
-			if (this->get() != other.get()) //应该去指针，上面那样的话，类型完全不一样了
+			if (this->get() != other.get()) //应该取指针，上面那样的话，类型完全不一样了
 				reset(other.release());
 			return *this;
 		}
 
+		//非必要，其实泛型的copying函数就能解决问题了
+		//template<class U>
+		//operator auto_ptr<U>() { //让编译器能够进行隐式类型转换
+		//	return auto_ptr<U>(pointer_);
+		//}
+
 	private:
-		T * pointer_;
+		mutable T * pointer_;
 	};
 
 	template<class T>
